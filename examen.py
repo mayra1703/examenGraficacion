48ab0771645827291b63f5649ca86c95c194013c
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2023-10-01 20:23:35 -0700

    Validaciones

--- examen.py
+++ examen.py
@@ -11,12 +11,16 @@ from tkinter import *
 app = tk.Tk()
 
 # Configuración de la interfaz
-app.geometry('800x400')
+app.geometry('1000x500')
 app.title('Sombreado de un Poligono')
 
 # Variables para el tamaño del lienzo
 ancho_var = tk.StringVar()
 alto_var = tk.StringVar()
+
+# Variables de condicion
+canva_creado = False
+poligono_creado = False
 
 # Coordenadas de los vértices del polígono
 vertices = []
@@ -24,65 +28,83 @@ vertices = []
 
 # Creación de Frames
 frame1 = tk.Frame(app, bg='white')
-frame2 = tk.Frame(app, bg='#F2545B')
+frame2 = tk.Frame(app, bg='#006d77')
 
 # Función del botón "Crear Canva"
 def dibujarFigura():
-    tk.Label(frame2, fg='white', bg='#F2545B').pack()
 
-    ancho = int(ancho_var.get())
-    alto = int(alto_var.get())
+    global canva_creado
 
-    # Definir un lienzo
-    canvas = PIL.Image.new('RGB', (ancho, alto), (255, 255, 255))
+    if not canva_creado:
+        tk.Label(frame2, fg='white', bg='#006d77').pack()
 
-    tkpic = ImageTk.PhotoImage(canvas)
-    label = tk.Label(frame2, image=tkpic)
-    label.image = tkpic  # Guardar una referencia a la imagen para evitar que se elimine
-    label.pack()
-    app.geometry(f"{ancho + 300}x{alto + 100}")
+        ancho = int(ancho_var.get())
+        alto = int(alto_var.get())
 
-    def callback(event):
-            color = (0, 0, 0)
-            gl.pointAround(canvas, event.x, event.y, (ancho, alto), color)
-            tkpic = ImageTk.PhotoImage(canvas)
-            label.config(image=tkpic)
-            label.image = tkpic  # Save reference to image
-            vertices.append((event.x, event.y))
-
-    label.bind("<Button-1>", callback)
-
-    def crearPoligono():
-        relleno = colorchooser.askcolor(title="Choose color")
-        color = tuple(int(c) for c in relleno[0])
-        print(color)
-
-        gl.drawPolygon(gl.matrixToCartessian(vertices, ancho, alto), (0, 0, 0), canvas)
-        gl.drawGradientPolygon(gl.matrixToCartessian(vertices, ancho, alto), color, canvas)
+        # Definir un lienzo
+        canvas = PIL.Image.new('RGB', (ancho, alto), (255, 255, 255))
 
         tkpic = ImageTk.PhotoImage(canvas)
-        label.config(image=tkpic)
-        label.image = tkpic  # Save reference to image
+        label = tk.Label(frame2, image=tkpic)
+        label.image = tkpic  # Guardar una referencia a la imagen para evitar que se elimine
         label.pack()
+        app.geometry(f"{ancho + 500}x{alto + 100}")
 
         def callback(event):
-            centroid = (event.x, event.y)
-            gl.drawGradientPolygon(gl.matrixToCartessian(vertices, ancho, alto), color, canvas, centroid)
-            tkpic = ImageTk.PhotoImage(canvas)
-            label.config(image=tkpic)
-            label.image = tkpic  # Save reference to image
+                color = (0, 0, 0)
+                gl.pointAround(canvas, event.x, event.y, (ancho, alto), color)
+                tkpic = ImageTk.PhotoImage(canvas)
+                label.config(image=tkpic)
+                label.image = tkpic  # Save reference to image
+                vertices.append((event.x, event.y))
 
         label.bind("<Button-1>", callback)
-  
-    tk.Button(
-        frame1,
-        text='Crear Poligono',
-        font=('Courier', 10),
-        bg='#F2545B',
-        fg='white',
-        command=crearPoligono,
-    ).pack()
-  
+
+        def crearPoligono():
+            
+            global poligono_creado
+            
+            if not poligono_creado:
+                relleno = colorchooser.askcolor(title="Choose color")
+                color = tuple(int(c) for c in relleno[0])
+                print(color)
+
+                gl.drawPolygon(gl.matrixToCartessian(vertices, ancho, alto), (0, 0, 0), canvas)
+                gl.drawGradientPolygon(gl.matrixToCartessian(vertices, ancho, alto), color, canvas)
+
+                tkpic = ImageTk.PhotoImage(canvas)
+                label.config(image=tkpic)
+                label.image = tkpic  # Save reference to image
+                label.pack()
+
+                def callback(event):
+                    centroid = (event.x, event.y)
+                    gl.drawGradientPolygon(gl.matrixToCartessian(vertices, ancho, alto), color, canvas, centroid)
+                    tkpic = ImageTk.PhotoImage(canvas)
+                    label.config(image=tkpic)
+                    label.image = tkpic  # Save reference to image
+
+                label.bind("<Button-1>", callback)
+                poligono_creado = True
+    
+        tk.Label(
+            frame1,
+            text='Ahora puedes dibujar los puntos de tu poligono en el canva!',
+            fg='black',
+            bg='white',
+        ).pack(pady=10)
+        
+        tk.Button(
+            frame1,
+            text='Crear Poligono',
+            font=('Courier', 10),
+            bg='#006d77',
+            fg='white',
+            command=crearPoligono,
+        ).pack()
+
+        canva_creado = True
+    
 # Creacion de elementos
 tk.Label(
     frame1,
@@ -124,16 +146,16 @@ tk.Button(
     frame1,
     text='Crear Canva',
     font=('Courier', 10),
-    bg='#F2545B',
+    bg='#006d77',
     fg='white',
     command=dibujarFigura,
-).pack()
+).pack(pady=10)
 
 tk.Label(
     frame2,
     text='Canva',
     fg='white',
-    bg='#F2545B',
+    bg='#006d77',
     font=('Arial', 17)
 ).pack(pady=10)
 
